cb9f0ba100c443910a050092c5156e9ac7f43c39
Alex Trushev <alex.trushev95@gmail.com>
2022-12-14 20:47:10 +0100

    Add form

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const idNumber = Math.round(Math.random() * 1000000).toString();
+    const expenseData = {
+      ...enteredExpenseData,
+      id: idNumber,
+    };
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
